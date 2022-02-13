63d4eb134c74d6421f2b4d1272261c3a131e7a66
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2022-02-13 17:21:52 -0300

    Criando um método para obtermos os benefícios de reaproveitamento e delegação

--- poo/introducao-a-poo/01-resolvendo-um-problema-sem-poo/src/application/Program.java
+++ poo/introducao-a-poo/01-resolvendo-um-problema-sem-poo/src/application/Program.java
@@ -20,11 +20,11 @@ public class Program {
 
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
-		Triangle x,y;
+
+		Triangle x, y;
 		x = new Triangle();
 		y = new Triangle();
-		
+
 		System.out.println("Enter the measures of triangle X: ");
 		x.a = sc.nextDouble();
 		x.b = sc.nextDouble();
@@ -33,24 +33,21 @@ public class Program {
 		y.a = sc.nextDouble();
 		y.b = sc.nextDouble();
 		y.c = sc.nextDouble();
-		
-		double p = (x.a + x.b + x.c)/2.0;
-		double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
-		
-		p = (y.a + y.b + y.c)/2.0;
-		double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
-		
+
+		double areaX = x.area();
+
+		double areaY = y.area();
+
 		System.out.printf("Triangle X area: %.4f%n", areaX);
 		System.out.printf("Triangle Y area: %.4f%n", areaY);
-		
-		if(areaX > areaY) {
+
+		if (areaX > areaY) {
 			System.out.println("Large area: X");
-		}else {
+		} else {
 			System.out.println("Large area: Y");
 		}
-		
+
 		sc.close();
 	}
 
 }
-
