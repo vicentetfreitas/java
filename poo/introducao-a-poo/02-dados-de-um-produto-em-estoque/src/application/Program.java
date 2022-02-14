b73cd40d82925b5b43de8ba62e341ba41110ef22
Vicente Freitas <vicenteteixeirafreitas@gmail.com>
2022-02-14 20:07:55 -0300

    Finalizando o programa

--- poo/introducao-a-poo/02-dados-de-um-produto-em-estoque/src/application/Program.java
+++ poo/introducao-a-poo/02-dados-de-um-produto-em-estoque/src/application/Program.java
@@ -21,8 +21,24 @@ public class Program {
 		System.out.print("Quantity in stock: ");
 		product.quantity = sc.nextInt();
 		
-		System.out.println(product);
+		System.out.println();
+		System.out.println("Product data: " + product);
+		System.out.println();
+		
+		System.out.print("Enter the number of products to be added in stock: ");
+		int quantity = sc.nextInt();
+		product.addProducts(quantity);
 
+		System.out.println();
+		System.out.print("Update data: " + product);
+		System.out.println();
+		
+		System.out.print("Enter the number of products to be remove in stock: ");
+		quantity = sc.nextInt();
+		product.removeProducts(quantity);
+		
+		System.out.println();
+		System.out.println(product);
 		sc.close();
 
 	}
